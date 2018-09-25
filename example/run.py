eda62fea41650b672b0275f234fb8f8663177f89
Ahmad Faiyaz <faiyaz26@gmail.com>
2018-09-25 11:54:23 +0800

    Change status when model run started

--- example/run.py
+++ example/run.py
@@ -1,6 +1,7 @@
 from rm_sdk.tracking.entities import ModelRun
 
-with ModelRun(43) as model_run:
+
+with ModelRun(25) as model_run:
     model_run.log_param('abc', 'pqa')
     model_run.log_param('pq', '2324234')
     model_run.log_metric('mete1', 'wewrwe')
