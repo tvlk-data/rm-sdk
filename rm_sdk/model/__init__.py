d17612ebe91b214ed893843fa04af48f2a54b31f
Ahmad Faiyaz <faiyaz26@gmail.com>
2018-09-27 21:32:33 +0800

    Fix model upload path

--- rm_sdk/model/__init__.py
+++ rm_sdk/model/__init__.py
@@ -8,6 +8,6 @@ def upload_model_directory(run_id, local_dir, destination_dir=None):
     model_name = 'ters' # giving some random name, need to fix it
     store = GoogleCloudStorage(MODEL_UPLOAD_BASE_PATH)
 
-    model_upload_relative_path = "%s/%s" % (model_name, run_id)
+    model_upload_relative_path = "%s/%s/models" % (model_name, run_id)
 
     store.log_artifacts(local_dir, model_upload_relative_path)
