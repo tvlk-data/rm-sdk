94dbd3f7970620ac246c3864029498d632986bb7
Ahmad Faiyaz <faiyaz26@gmail.com>
2018-10-03 12:31:38 +0800

    Add example of relative path support

--- example/entity/run.py
+++ example/entity/run.py
@@ -9,5 +9,5 @@ with ModelRun("c9c279c4-efc5-48f0-bbf3-68c44dcffbc7", base_path) as model_run:
     model_run.log_param('pq', '2324234')
     model_run.log_metric('mete1', 'wewrwe')
     model_run.log_metric('sdfqqw', 'sdfwerew')
-    #model_run.upload_model_directory(model_path)
+    #model_run.upload_model_directory('/outputs/models')
     model_run.upload_model_directory()
