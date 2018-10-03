1c9bed2e6996521b65a31184e3c346f9e2926992
Ahmad Faiyaz <faiyaz26@gmail.com>
2018-10-03 12:29:43 +0800

    Add base path support

--- example/entity/run.py
+++ example/entity/run.py
@@ -2,12 +2,12 @@ import os
 from rm_sdk.entities import ModelRun
 
 
-with ModelRun("3849e808-dc79-4888-bfda-c6c38462f6d2") as model_run:
+base_path = os.path.dirname(os.path.realpath(__file__))
+
+with ModelRun("c9c279c4-efc5-48f0-bbf3-68c44dcffbc7", base_path) as model_run:
     model_run.log_param('abc', 'pqa')
     model_run.log_param('pq', '2324234')
     model_run.log_metric('mete1', 'wewrwe')
     model_run.log_metric('sdfqqw', 'sdfwerew')
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    model_path = dir_path + '/outputs/models'
     #model_run.upload_model_directory(model_path)
     model_run.upload_model_directory()
--- rm_sdk/model/__init__.py
+++ rm_sdk/model/__init__.py
@@ -7,4 +7,6 @@ MODEL_UPLOAD_BASE_PATH = os.getenv('MODEL_UPLOAD_URI', 'gs://rm-run-outputs')
 def upload_model_directory(model_run, local_dir, destination_dir=None):
     store = GoogleCloudStorage(MODEL_UPLOAD_BASE_PATH)
     model_upload_relative_path = "%s/%s/models" % (model_run.model_name, model_run.runId)
+    if destination_dir:
+        model_upload_relative_path = destination_dir
     store.log_artifacts(local_dir, model_upload_relative_path)
