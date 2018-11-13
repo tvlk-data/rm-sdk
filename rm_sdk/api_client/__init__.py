68b1b69763b9bb8f7c0c171da3383e10758b7597
Ahmad Faiyaz <faiyaz26@gmail.com>
2018-11-13 14:20:38 +0800

    Fetch auth0 config from GS uri

--- rm_sdk/api_client/__init__.py
+++ rm_sdk/api_client/__init__.py
@@ -1,23 +1,47 @@
 import os
 import http.client
 import json
+import base64
 
+import googleapiclient.discovery
 from graphqlclient import GraphQLClient
 from retrying import retry
 
+from rm_sdk.storage import GoogleCloudStorage
+
 
 GRAPHQL_API_URL_ENV = 'RM_GRAPHQL_API_URL'
+AUTH0_CONFIG_URI = 'gs://rm-config/auth0-stg.json.enc'
 
 def retry_if_exception(exception):
     print("Got some exception during HTTP request from rm-sdk. Retrying..")
     return True
 
 @retry(retry_on_exception=retry_if_exception, stop_max_attempt_number=3, wait_random_min=1000, wait_random_max=2000)
+def get_auth0_config(project_id='tvlk-data-dev-179204',location_id="global",
+                key_ring_id='rm-config', crypto_key_id='rm-config'):
+    storage = GoogleCloudStorage(AUTH0_CONFIG_URI)
+    blob = storage.get_artifact()
+    kms_client = googleapiclient.discovery.build('cloudkms', 'v1')
+    name = 'projects/{}/locations/{}/keyRings/{}/cryptoKeys/{}'.format(
+        project_id, location_id, key_ring_id, crypto_key_id)
+    ciphertext = blob.download_as_string()
+    crypto_keys = kms_client.projects().locations().keyRings().cryptoKeys()
+    request = crypto_keys.decrypt(
+        name=name,
+        body={'ciphertext': base64.b64encode(ciphertext).decode('ascii')})
+    response = request.execute()
+    plaintext = base64.b64decode(response['plaintext'].encode('ascii'))
+    return json.loads(plaintext)
+
+
+@retry(retry_on_exception=retry_if_exception, stop_max_attempt_number=3, wait_random_min=1000, wait_random_max=2000)
 def get_auth0_token():
-    AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
-    AUTH0_RESOURCE_SERVER = os.getenv('AUTH0_RESOURCE_SERVER')
-    AUTH0_CLIENT_ID  = os.getenv('AUTH0_CLIENT_ID')
-    AUTH0_CLIENT_SECRET = os.getenv('AUTH0_CLIENT_SECRET')
+    auth0_data = get_auth0_config()
+    AUTH0_DOMAIN = auth0_data['domain']
+    AUTH0_RESOURCE_SERVER = auth0_data['audience']
+    AUTH0_CLIENT_ID  = auth0_data['client_id']
+    AUTH0_CLIENT_SECRET = auth0_data['client_secret']
 
     conn = http.client.HTTPSConnection(AUTH0_DOMAIN)
     payload = "{\"client_id\":\"%s\",\"client_secret\":\"%s\",\"audience\":\"%s\",\"grant_type\":\"client_credentials\"}" \
@@ -40,7 +64,6 @@ class RMGraphQLClientBase(object):
         if not token:
             token = get_auth0_token()
         bearer_token = 'Bearer '+ token
-        
         self.client.inject_token(bearer_token)
 
     
